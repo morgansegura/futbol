b9098176819a46ec8a27cfb4272c19b5501996dc
Morgan Segura <morgansegura@gmail.com>
2019-05-30 07:22:15 -0700

    Updates

---
+++ precache-manifest.b339ad93556c2833613ae9985515d9f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204cea3c1284bec85ae89d1cb378a3b0",
+    "url": "/futbol/index.html"
+  },
+  {
+    "revision": "aec12a4507fba4500e6f",
+    "url": "/futbol/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "3de30be8dea38f9f2202",
+    "url": "/futbol/static/css/main.b46bd67e.chunk.css"
+  },
+  {
+    "revision": "aec12a4507fba4500e6f",
+    "url": "/futbol/static/js/2.151dce15.chunk.js"
+  },
+  {
+    "revision": "3de30be8dea38f9f2202",
+    "url": "/futbol/static/js/main.af14e924.chunk.js"
+  },
+  {
+    "revision": "5d92f7a82d2163bd1563",
+    "url": "/futbol/static/js/runtime~main.aad00cea.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/futbol/static/media/SEGA.12a3db8b.woff"
+  }
+]);
